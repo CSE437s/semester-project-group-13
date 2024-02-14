641648cfd712d0ce96c227b4a8c4a0af175b287c
Chris Lopez <chris.bradyl26@gmail.com>
2024-02-13 19:20:35 -0600

    be query

--- backend/index.js
+++ backend/index.js
@@ -25,3 +25,28 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("Connected successfully to MySql server")
 });
+
+
+
+app.post("/user", (req, res) => {
+
+const username = req.body.username;
+const password = req.body.password;
+
+const selectQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+connection.query(selectQuery, [username, password], (err, results) => {
+    if (err) {
+        console.error("Error checking credentials:", err);
+        res.status(500).send("Error checking credentials");
+    } else {
+        if (results.length > 0) {
+            console.log("Credentials are valid");
+            res.status(200).send("Credentials are valid");
+        } else {
+            console.log("Invalid credentials");
+            res.status(401).send("Invalid credentials");
+        }
+    }
+});
+});
