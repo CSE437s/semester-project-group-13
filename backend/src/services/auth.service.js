8d2e14e964a57c4614c088c84cb8623d2207d862
Chris Lopez <chris.bradyl26@gmail.com>
2024-03-05 18:26:44 -0600

    login works now

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -10,17 +10,17 @@ async function logout() {
     }
   }
 
-async function login(username, password) {
-  try {
-    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
-
-    const rows = await db.query(sql, [username, password]);
-    return { data: rows[0] };
-  } catch (error) {
-    console.error('Error while logging in', error);
-    throw error;
+  async function login(username, password) {
+    try {
+      const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+      const rows = await db.query(sql, [username, password]);
+      
+      return { data: rows };
+    } catch (error) {
+      console.error('Error while logging in', error);
+      throw error;
+    }
   }
-}
 
 module.exports = {
   login,
