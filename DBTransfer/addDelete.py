bbdf231cc163ae6a09dd099ffd57bad943c7892c
Chris Lopez <chris.bradyl26@gmail.com>
2024-04-05 18:55:35 -0500

    Added Deleted Columns

---
+++ DBTransfer/addDelete.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import json
+
+# Define your database connection parameters
+config = {
+    'user': 'avnadmin',
+    'port': 13780,
+    'password': 'AVNS_UugTJpwoWZiGpP6VjcA',
+    'host': 'oasisdb-oasisdb.a.aivencloud.com',
+    'database': 'oasis',
+    'raise_on_warnings': True
+}
+
+try:
+    # Establish a connection to the database
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+
+    # Define the table creation query
+    create_table_query = """
+    ALTER TABLE users
+    ADD COLUMN is_deleted BOOLEAN DEFAULT FALSE;
+    """
+
+    # Execute the table creation query
+    cursor.execute(create_table_query)
+
+    # Commit changes and close the cursor and connection
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    print("Table created successfully.")
+except mysql.connector.Error as err:
+    print("Error:", err)
